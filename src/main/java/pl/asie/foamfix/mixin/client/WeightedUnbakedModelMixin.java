a5e706ef6a6219c9c7157b8427e5c4d4268ffe5d
Chocohead <Chocohead@Aroma1997.org>
2020-06-01 02:39:22 +0100

    Use smaller list implementations for common cases
    Often a variant list only includes one variant, so it's better to save making an array in that case

--- src/main/java/pl/asie/foamfix/mixin/client/WeightedUnbakedModelMixin.java
+++ src/main/java/pl/asie/foamfix/mixin/client/WeightedUnbakedModelMixin.java
@@ -28,15 +28,21 @@
 package pl.asie.foamfix.mixin.client;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.google.common.collect.Iterables;
 
 import net.minecraft.client.renderer.model.Variant;
 import net.minecraft.client.renderer.model.VariantList;
+import net.minecraft.client.renderer.model.VariantList.Deserializer;
 
 @Mixin(VariantList.class)
 abstract class WeightedUnbakedModelMixin {
@@ -44,6 +50,23 @@ abstract class WeightedUnbakedModelMixin {
 	private void shrink(List<Variant> variants, CallbackInfo info) {
 		if (variants instanceof ArrayList) {
 			((ArrayList<?>) variants).trimToSize();
+		}
+	}
+
+	@Mixin(Deserializer.class)
+	static abstract class DeserialiserMixin {
+		@ModifyArg(method = "deserialize", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/model/VariantList;<init>(Ljava/util/List;)V", ordinal = 0))
+		private List<Variant> shrink(List<Variant> variants) {
+			switch (variants.size()) {
+			case 0:
+				return Collections.emptyList();
+
+			case 1:
+				return Collections.singletonList(Iterables.getOnlyElement(variants));
+
+			default:
+				return Arrays.asList(variants.toArray(new Variant[0]));
+			}
 		}
 	}
 }
